08a77994afdf3bb4a5eef4563d883b3c47909504
sh1var3ddy <srajamuri@gmail.com>
2024-08-29 15:27:06 -0400

    Added http status codes for problem service

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,23 +1,25 @@
+const {StatusCodes} = require('http-status-codes');
+
 function pingProblemController(req,res){
     return res.json({message:"Ping check from ping problem controller"});
 }
 
 function addProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
 }
 
 function getProblem(req,res){
-    
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
 }
 
 function getProblems(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
 }
 function updateProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
 }
 function deleteProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
 }
 
 module.exports = {
