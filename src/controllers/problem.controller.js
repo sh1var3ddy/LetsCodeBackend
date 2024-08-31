7b4dacfbc6597c552acc45b8008b97346b6e473f
sh1var3ddy <srajamuri@gmail.com>
2024-08-30 23:05:35 -0400

    Feat: Added error handle middleware

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,11 +1,16 @@
 const {StatusCodes} = require('http-status-codes');
+const NotImplemented = require('../errors/notimplemented.error');
 
 function pingProblemController(req,res){
     return res.json({message:"Ping check from ping problem controller"});
 }
 
-function addProblem(req,res){
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
+function addProblem(req,res,next){
+    try{
+        throw new NotImplemented('addProblem');
+    }catch(error){
+        next(error);
+    }
 }
 
 function getProblem(req,res){
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser  = require('body-parser');
 
 const {PORT} =require("./config/server.config");
 const apiRouter = require('./routes/index');
+const errorHandler = require('./utilities/errorHandler');
 const app = express();
 
 app.use(bodyParser.json());
@@ -14,6 +15,8 @@ app.use("/api",apiRouter)
 app.get("/ping",(req,res)=>{
     return res.json({Message:"Hello from ping"})
 })
+// last middleware if any error comes
+app.use(errorHandler);
 app.listen(PORT,()=>{
-    console.log(`Server is running at PORT ${PORT}`)
+    console.log(`Server is running at PORT ${PORT}`);
 })
